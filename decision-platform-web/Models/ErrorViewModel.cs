15b900bce395b2cb4e2b0e79d37fd21af84ee229
Ivan Vashchilo <vashchiloivan@gmail.com>
2024-08-22 14:13:49 +0300

    Initial commit

---
+++ decision-platform-web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace decision_platform_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
