d4f5df9b4055f01b0d695caaf517e887ca16a55a
Ivan Vashchilo <vashchiloivan@gmail.com>
2024-09-03 19:34:46 +0300

    Импорт экспорт модели

--- DecisionPlatformWeb/Controllers/ImportExportController.cs
+++ DecisionPlatformWeb/Controllers/ImportExportController.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using System.Xml;
+using DecisionPlatformWeb.Entity;
+using DecisionPlatformWeb.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DecisionPlatformWeb.Controllers;
@@ -7,57 +11,57 @@ namespace DecisionPlatformWeb.Controllers;
 // TODO excel models??
 public class ImportExportController: Controller
 {
-    // [HttpPost("export-xml")]
-    // public IActionResult ExportXml([FromBody] TaskCondition taskCondition)
-    // {
-    //     var stream = new MemoryStream();
-    //     XmlDocument document = new XmlDocument();
-    //     XmlNode xmlNode = XmlRepositoryUtils.ObjectToXml(document, taskCondition);
-    //     document.AppendChild(xmlNode);
-    //     document.Save(stream);
-    //
-    //     stream.Position = 0;
-    //     var a = new FileStreamResult(stream, "text/plain")
-    //     {
-    //         FileDownloadName = "model.xml"
-    //     };
-    //     return a;
-    // }
-    //
-    // [HttpPost("export-json")]
-    // public IActionResult ExportJson([FromBody] TaskCondition taskCondition)
-    // {
-    //     MemoryStream stream = new MemoryStream();
-    //     JsonSerializer.Serialize(stream, taskCondition);
-    //
-    //     stream.Position = 0;
-    //     var a = new FileStreamResult(stream, "text/plain")
-    //     {
-    //         FileDownloadName = "model.json"
-    //     };
-    //     return a;
-    // }
-    //
-    // [HttpPost("import")]
-    // public IActionResult Import(IFormFile file)
-    // {
-    //     int nameLength = file.FileName.Length;
-    //     if (file.FileName[(nameLength - 4)..].Equals(".xml"))
-    //     {
-    //         var stream = file.OpenReadStream();
-    //         XmlDocument document = new XmlDocument();
-    //         document.Load(stream);
-    //         TaskCondition taskCondition = XmlRepositoryUtils.XmlNodeToObject<TaskCondition>(document.SelectSingleNode("TaskCondition"),
-    //             "TaskCondition");
-    //         return Json(taskCondition);
-    //     }
-    //
-    //     if (file.FileName[(nameLength - 5)..].Equals(".json"))
-    //     {
-    //         TaskCondition? taskCondition = JsonSerializer.Deserialize<TaskCondition>(file.OpenReadStream());
-    //         return Json(taskCondition);
-    //     }
-    //     
-    //     return Json(new TaskCondition());
-    // }
+    [HttpPost("export-xml")]
+    public IActionResult ExportXml([FromBody] TaskCondition taskCondition)
+    {
+        var stream = new MemoryStream();
+        XmlDocument document = new XmlDocument();
+        XmlNode xmlNode = XmlRepositoryUtils.ObjectToXml(document, taskCondition);
+        document.AppendChild(xmlNode);
+        document.Save(stream);
+    
+        stream.Position = 0;
+        var a = new FileStreamResult(stream, "text/plain")
+        {
+            FileDownloadName = "model.xml"
+        };
+        return a;
+    }
+    
+    [HttpPost("export-json")]
+    public IActionResult ExportJson([FromBody] TaskCondition taskCondition)
+    {
+        MemoryStream stream = new MemoryStream();
+        JsonSerializer.Serialize(stream, taskCondition);
+    
+        stream.Position = 0;
+        var a = new FileStreamResult(stream, "text/plain")
+        {
+            FileDownloadName = "model.json"
+        };
+        return a;
+    }
+    
+    [HttpPost("import")]
+    public IActionResult Import(IFormFile file)
+    {
+        int nameLength = file.FileName.Length;
+        if (file.FileName[(nameLength - 4)..].Equals(".xml"))
+        {
+            var stream = file.OpenReadStream();
+            XmlDocument document = new XmlDocument();
+            document.Load(stream);
+            TaskCondition taskCondition = XmlRepositoryUtils.XmlNodeToObject<TaskCondition>(document.SelectSingleNode("TaskCondition"),
+                "TaskCondition");
+            return Json(taskCondition);
+        }
+    
+        if (file.FileName[(nameLength - 5)..].Equals(".json"))
+        {
+            TaskCondition? taskCondition = JsonSerializer.Deserialize<TaskCondition>(file.OpenReadStream());
+            return Json(taskCondition);
+        }
+        
+        return Json(new TaskCondition());
+    }
 }
