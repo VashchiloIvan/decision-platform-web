b1c44a8365fd10347ee66419727edfb0aeaa557e
Иван Ващило <i.vashchilo@vk.team>
2025-04-27 11:16:44 +0300

    Добавлена поддержка методов из PowerIndeces

---
+++ DecisionPlatformWeb/Entity/PowerIndeces/PowerIndecesRequest.cs
@@ -0,0 +1,12 @@
+﻿using PowerIndecesDecisionWrapperCsharp;
+
+namespace DecisionPlatformWeb.Entity.PowerIndeces
+{
+    public class CalculateRequest
+    {
+        public double Quota { get; set; }
+        public int QuotaType { get; set; }
+        public int CalcType { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
---
+++ DecisionPlatformWeb/Service/Solver/PowerIndecesSolver.cs
@@ -0,0 +1,57 @@
+﻿using PowerIndecesDecisionWrapperCsharp;
+
+namespace DecisionPlatformWeb.Service.Solver
+{
+    public class PowerIndecesSolver
+    {
+        private DirectCalc DirectCalcService { get; set; }
+        private GenerCalc GenerCalcService { get; set; }
+
+
+        public PowerIndecesSolver()
+        {
+            DirectCalcService = new DirectCalc();
+            GenerCalcService = new GenerCalc();
+        }
+
+        public PowerIndexs CalcSSDirectEquiles(List<Player> players, double quota)
+        {
+            return DirectCalcService.powerIndexShapleyShubick(new Players(players), (int)QuotaType.EQUALITY, quota);
+        }
+
+        public PowerIndexs CalcSSDirectExceeding(List<Player> players, double quota)
+        {
+            return DirectCalcService.powerIndexShapleyShubick(new Players(players), (int)QuotaType.EXCEEDING, quota);
+        }
+
+        public PowerIndexs CalcBDirectEquiles(List<Player> players, double quota)
+        {
+            return DirectCalcService.powerIndexBanzhaf(new Players(players), (int)QuotaType.EQUALITY, quota);
+        }
+
+        public PowerIndexs CalcBDirectExceeding(List<Player> players, double quota)
+        {
+            return DirectCalcService.powerIndexBanzhaf(new Players(players), (int)QuotaType.EXCEEDING, quota);
+        }
+
+        public PowerIndexs CalcSSGenerEquiles(List<Player> players, double quota)
+        {
+            return GenerCalcService.powerIndexShapleyShubick(new Players(players), (int)QuotaType.EQUALITY, quota);
+        }
+
+        public PowerIndexs CalcSSGenerExceeding(List<Player> players, double quota)
+        {
+            return GenerCalcService.powerIndexShapleyShubick(new Players(players), (int)QuotaType.EXCEEDING, quota);
+        }
+
+        public PowerIndexs CalcBGenerEquiles(List<Player> players, double quota)
+        {
+            return GenerCalcService.powerIndexBanzhaf(new Players(players), (int)QuotaType.EQUALITY, quota); 
+        }
+
+        public PowerIndexs CalcBGenerExceeding(List<Player> players, double quota)
+        {
+            return GenerCalcService.powerIndexBanzhaf(new Players(players), (int)QuotaType.EXCEEDING, quota);
+        }
+    }
+}
